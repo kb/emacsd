c32d536cc451d4c1be0c11874b8ea17085501999
Kyle Bolton <kyle.bolton@gmail.com>
2010-02-19 11:53:24 -0500

    Rinari-mode and dependencies

---
+++ elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,16 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19326 49129
+;;;;;;  854894))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; findr-autoloads.el ends here
