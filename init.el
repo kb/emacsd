7e0bbc12c54fe6abec683a7ab223520877e3a627
Kyle Bolton <kyle.bolton@gmail.com>
2010-02-19 11:33:22 -0500

    oh hai elpa. Plz manage mi pckagez.

--- init.el
+++ init.el
@@ -16,3 +16,13 @@
 ; --- DISABLE BACKUPS
 (setq backup-directory-alist (quote ((".*" . "~/.emacs_temp/backups"))))
 (setq auto-save-file-name-transforms `(("\\(?:[^/]*/\\)*\\(.*\\)" ,"~/.emacs_temp/autosaves" t)))
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
