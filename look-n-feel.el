038b3facf76b5b1f6f5666262ba95a9d5f963544
Kyle Bolton <kyle@crashlytics.com>
2012-02-03 11:10:20 -0500

    custom-set-faces for magit

--- look-n-feel.el
+++ look-n-feel.el
@@ -26,4 +26,19 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(magit-branch ((t (:inherit magit-header :background "wheat4" :foreground "DarkBlue" :box (:line-width 2 :color "wheat3" :style released-button) :weight bold :height 100))))
+ '(magit-diff-add ((nil (:foreground "green"))))
+ '(magit-diff-file-header ((t (:inherit magit-header :foreground "white"))))
+ '(magit-diff-hunk-header ((t (:inherit magit-header :background "Grey13" :foreground "khaki1" :slant italic))))
+ '(magit-header ((t (:foreground "white" :height 1))))
+ '(magit-item-highlight ((((class color) (background dark)) (:background "gray10"))))
+ '(magit-section-title ((t (:inherit magit-header :foreground "yellow" :underline t :weight bold :height 150))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1)))))
+
 (provide 'look-n-feel)
