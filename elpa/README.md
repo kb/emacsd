386a2a23b04beb49606ce5aaea13a717afcf1fc3
Kyle Bolton <kyle.bolton@gmail.com>
2013-09-24 06:00:37 -0400

    Remove ELPA packages from source control
    Maintaining ELPA packages via source control usually ends up in a world
    of hurt. Instead here's a list of useful packages to be installed by
    hand via EPLA.

---
+++ elpa/README.md
@@ -0,0 +1,9 @@
+# Prefered Packages
+
+Maintaining ELPA packages via source control usually ends up in a world of hurt. Instead here's a list of useful packages to be installed by hand via EPLA.
+
+* ruby-mode
+* magit
+* magit-commit-training-wheels
+* multiple-cursors
+* motion-mode
--- elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19326 49129
-;;;;;;  854894))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; findr-autoloads.el ends here
